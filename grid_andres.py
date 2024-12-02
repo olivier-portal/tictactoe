898090803664c876fe1174d3895c88644efbcfe0
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2024-12-02 11:33:15 +0100

    Changes in Andres' grid

--- grid_andres.py
+++ grid_andres.py
@@ -1,1 +1,12 @@
-grid = [[0,0,0],[0,0,0],[0,0,0]]
+grid = [[1,2,3],[4,5,6],[7,8,9]]
+
+def display_grid():
+    print(f" {grid[0][0]} | {grid[0][1]} | {grid[0][2]} ")
+    print("-----------")
+    print(f" {grid[1][0]} | {grid[1][1]} | {grid[1][2]} ")
+    print("-----------")
+    print(f" {grid[2][0]} | {grid[2][1]} | {grid[2][2]} ")
+    
+
+grid = display_grid()
+print(grid)
