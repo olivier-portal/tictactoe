36e2e01884df908e0dc3c7887e79c873ecdb4189
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2024-12-02 16:29:40 +0100

    first steps of winner_checker function

--- grid_andres.py
+++ grid_andres.py
@@ -1,13 +1,10 @@
-grid = [[1,2,3],[4,5,6],[7,8,9]]
-
 def display_grid():
     print(f" {grid[0][0]} | {grid[0][1]} | {grid[0][2]} ")
-    print("-----------")
+    print("---+---+---")
     print(f" {grid[1][0]} | {grid[1][1]} | {grid[1][2]} ")
-    print("-----------")
+    print("---+---+---")
     print(f" {grid[2][0]} | {grid[2][1]} | {grid[2][2]} ")
-    
 
-grid = display_grid()
-print(grid)
+grid = [[1,2,3],[4,5,6],[7,8,9]]
+display_grid()
 
---
+++ winner_checker.py
@@ -0,0 +1,29 @@
+board = [["X", " ", "X"],
+        [" ", " ", "X"],
+        ["O", "X", "X"]]
+
+win_checker = False
+
+while win_checker == False:
+    if board[0][0] == "X" and board[0][1] == "X" and board[0][2] == "X":
+        print("X ha ganado con 1ra línea horizontal")
+        win_checker = True
+    elif board[1][0] == "X" and board[1][1] == "X" and board[1][2] == "X":
+        print("X ha ganado con 2da línea horizontal")
+        win_checker = True
+    elif board[2][0] == "X" and board[2][1] == "X" and board[2][2] == "X":
+        print("X ha ganado con 3ra línea horizontal")
+        win_checker = True
+    elif board[0][0] == "X" and board[1][0] == "X" and board[2][0] == "X":
+        print("X ha ganado con 1ra línea vertical")
+        win_checker = True
+    elif board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X":
+        print("X ha ganado con 2da línea vertical")
+        win_checker = True
+    elif board[0][2] == "X" and board[1][2] == "X" and board[2][2] == "X":
+        print("X ha ganado con 3ra línea vertical")
+        win_checker = True
+    else:
+        print("No has ganado")
+        win_checker = False
+        break
