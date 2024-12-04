e9869f0ce46206dc43e59cf996f257f8c5c06661
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-12-04 12:17:48 +0100

    Corrected mistakes in functions

--- fill_board.py
+++ fill_board.py
@@ -1,13 +1,17 @@
-from start_game_function import print_board
-
+# from start_game_function import print_board
+choice = [2, 2, "X"]
+board = [[' ', ' ', 'O'], [' ', ' ', ' '], [' ', ' ', ' ']]
 # Define player's turn and fill a box with the predefined symbol
 def fill_board(board, choice):
+
+
+
 
 # Updates the game board with the player symbol in the chosen box.
 #:board: Game board list (3x3).
 #:choice: player choice(row, column symbol).
-
-    row, column, symbol = choice
-    board[row][column] == " "
-    board[row][column] = symbol
-    return board
+    row = choice[0]
+    column = choice[1]
+    symbol = choice[2]
+    board[row - 1][column - 1] = symbol
+    return board 
